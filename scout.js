469299c03d5373e552ad3b048968b3e25e0bfa37
Mark Old <dlom234@gmail.com>
2014-01-28 12:43:22 -0800

    it works
    kinda
    
    added teleop stuffs and wired up buttons

--- scout.js
+++ scout.js
@@ -1,3 +1,29 @@
+var dataStack = [];
+var littleRobotX = -1;
+var littleRobotY = -1;
+
+var pushData = function(id, value, x, y) {
+	dataStack.push({
+		"action": id,
+		"value": value,
+		"x": x,
+		"y": y,
+		"time": Date.getTime()
+	});
+};
+
+var undoData = function() {
+	return dataStack.pop();
+};
+
+var consolidateData = function() {
+	var data = {};
+	dataStack.forEach(function(d) {
+		data[d.action] != null ? data[d.action] += d.value : data[d.action] = d.value;
+	});
+	return data;
+};
+
 var longPress = function(element, callback) {
 	var interval;
 	element.addEventListener("mousedown", function(e) {
@@ -13,12 +39,29 @@ var longPress = function(element, callback) {
 	});
 };
 
+var enableClassButtons = function(className, enabled) {
+    var buttons = document.querySelectorAll(".scout." + className);
+    for (var i = 0; i < buttons.length; i++) {
+        if (!buttons[i].classList.contains("always-on")) {
+            buttons[i].disabled = !enabled;
+            if (enabled) {
+            	buttons[i].classList.remove("disabled");
+            } else {
+            	buttons[i].classList.add("disabled");
+            }
+        }
+    }
+}
+
 var init = function(e) {
 	document.getElementById("start").addEventListener("click", function() {
 		document.getElementById("initial-overlay").classList.toggle("active");
-		document.getElementById("autonomous").classList.toggle("active");
+		document.getElementById("data-mode").classList.toggle("active");
 	});
 	document.getElementById("field-container").addEventListener("click", function(even) {
+		if (littleRobotX === -1 && littleRobotY === -1) {
+			enableClassButtons("collect", true);
+		}
 		fieldImg = document.getElementById("field");
 		var maxX = 742
 		var maxY = 338
@@ -36,11 +79,34 @@ var init = function(e) {
 		if (y < 25) y = 25;
 		littleRobot.style.left = (x - 25) + "px";
 		littleRobot.style.top = (y - 25) + "px";
+		littleRobotX = x;
+		littleRobotY = y;
 	});
 	longPress(document.getElementById("field-container"), function(even) {
 		even.preventDefault();
 		alert("you're a winner");
 	});
+	var buttons = document.querySelectorAll(".scout:not(#start)");
+    for (var i = 0; i < buttons.length; i++) {
+        var button = buttons[i];
+        enableClassButtons("eject", false);
+        enableClassButtons("collect", false);
+
+        button.addEventListener("click", function() {
+        	pushData(this.id, 1, littleRobotX, littleRobotY);
+            var gotBall = this.classList.contains("collect") ? true : false;
+            var ballStateChanged = !this.classList.contains("bad") && !this.classList.contains("always-on");
+            if (ballStateChanged) {
+                if (gotBall) {
+                    enableClassButtons("collect", false);
+                    enableClassButtons("eject", true);
+                } else {
+                    enableClassButtons("collect", true);
+                    enableClassButtons("eject", false);
+                }
+            }
+        });
+    }
 };
 
 window.addEventListener("load", init);

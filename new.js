cb68d0a84e01a1419da63e7c2ea7c072b337831c
AWOL <skunk.awol@gmail.com>
2014-02-01 11:42:54 -0800

    finished new

--- new.js
+++ new.js
@@ -1,7 +1,21 @@
 var data = {
     "modeTeleop": false,
-    "auto": {},
-    "teleop": {},
+
+    "auto": [],
+    "autoMeta": {
+        "deadBot": false,
+        "status": "noAuto",
+        "hotGoal": false,
+        "startPosition": {"x": 0, "y": 0},
+        "shootPosition": {"x": 0, "y": 0},
+        "finalPosition": {"x": 0, "y": 0}
+    },
+
+    "teleop": [],
+    "teleopMeta": {
+        "deadBot": false,
+    },
+
     "currentX": 100,
     "currentY": 169,
     "MAX_X": 742,
@@ -9,19 +23,92 @@ var data = {
 };
 
 $("#start").on("click", function(e) {
-    $("#initial-overlay")[0].classList.toggle("active");
-    $("#data-mode")[0].classList.toggle("active");
+    enableSelector("#initial-overlay", false);
+    enableSelector("#data-mode", true);
 });
 
 var littleRobot = spawnLittleRobot(data.currentX, data.currentY);
-$("#field-container")[0].appendChild(littleRobot);
+$("#field-container").appendChild(littleRobot);
 
-var fieldContainer = $("#field-container")[0];
+var fieldContainer = $("#field-container");
 
 onLongPress(fieldContainer, function(e) {
     console.log("long");
 });
 
 fieldContainer.on("click", function(e) {
-    // fill me in pls
+    var x = e.x;
+    var y = e.y;
+    if (x + 25 > data.MAX_X) x = data.MAX_X - 25;
+    if (y + 25 > data.MAX_Y) y = data.MAX_Y - 25;
+    if (x < 25) x = 25;
+    if (y < 25) y = 25;
+    data.currentX = x;
+    data.currentY = y;
+    littleRobot.style.left = (x - 25) + "px";
+    littleRobot.style.top = (y - 25) + "px"; // grid cause grace said so nothx
+});
+
+fieldContainer.on("mouseup", function(e) {
+    setTimeout(function() {
+        enableSelector("#field-container", false);
+        enableSelector("#scout-buttons", true);
+    }, 200);
+});
+
+// general
+
+$(".scout.toggle").on("click", function(e) {
+    var enabled = !data[data.modeTeleop ? "teleopMeta" : "autoMeta"][this.id];
+    data[data.modeTeleop ? "teleopMeta" : "autoMeta"][this.id] = enabled;
+    this.classList[enabled ? "add" : "remove"]("on");
+    this.classList[!enabled ? "add" : "remove"]("off");
+});
+
+$("#reset").on("click", function(e) {
+    document.location.reload(true);
+});
+
+// auto
+
+$(".scout.auto.position").on("click", function(e) {
+    data.autoMeta[this.id] = {"x": data.currentX, "y": data.currentY}; // FIX ME
+    enableSelector("#scout-buttons", false);
+    enableSelector("#field-container", true);
+});
+
+$(".scout.auto.status").on("click", function(e) {
+    data.autoMeta.status = this.id;
+    $(".scout.auto.status").each(function(element) {
+        element.classList.remove("selected");
+    });
+    this.classList.add("selected");
+});
+
+$("#auto-done").on("click", function(e) {
+    data.modeTeleop = true;
+    data.teleopMeta.deadBot = data.autoMeta.deadBot;
+    enableSelector("#auto", false);
+    enableSelector("#teleop", true);
+    this.classList.add("not-active");
+});
+
+// teleop
+
+$(".scout.teleop.collect").on("click", function(e) {
+    storeAction(this.id, 1, data.currentX, data.currentY); // FIX ME
+    enableSelector("#scout-buttons", false);
+    enableSelector("#field-container", true);
+    enableSelector("#collect", false);
+    enableSelector("#eject", true);
+});
+
+$(".scout.teleop.eject").on("click", function(e) {
+    storeAction(this.id, 1, data.currentX, data.currentY); // FIX ME
+    if (!this.classList.contains("bad")) {
+        enableSelector("#field-container", true);
+        enableSelector("#scout-buttons", false);
+        enableSelector("#eject", false);
+        enableSelector("#collect", true);
+    }
 });
--- util.js
+++ util.js
@@ -1,29 +1,39 @@
-var $ = document.querySelectorAll.bind(document);
-Element.prototype.on = Element.prototype.addEventListener;
-NodeList.prototype.on = function() {
-	var args = arguments;
-	this.forEach(function(element, i) {
-		Element.prototype.addEventListener.apply(element, args);
-	});
+/* jquery condensed into 22 lines and 148 times smaller */
+var $ = function() {
+    elementList = document.querySelectorAll.apply(document, arguments);
+    if (elementList.length === 1) elementList = elementList[0];
+    return elementList
 };
-NodeList.prototype.forEach = function(callback) {
+Element.prototype.on = Element.prototype.addEventListener;
+Element.prototype.each = function(callback) {
+    callback(this, 1, [this]);
+};
+NodeList.prototype.on = function() {
+    var args = arguments;
+    this.each(function(element, i) {
+        Element.prototype.on.apply(element, args);
+    });
+};
+NodeList.prototype.each = function(callback) {
     for (var i = 0; i < this.length; ++i) {
         var element = this[i];
         callback(element, i, this);
     }
 };
 
-var enableButtons = function(selector, enabled) {
-	$(selector).forEach(function(element) {
-		if (!element.classList.contains("always-on")) {
+var enableSelector = function(selector, enabled) {
+    $(selector).each(function(element) {
+        if (!element.classList.contains("always-on")) {
             element.disabled = !enabled;
             if (enabled) {
-                element.classList.remove("disabled");
+                element.classList.add("active");
+                element.classList.remove("not-active");
             } else {
-                element.classList.add("disabled");
+                element.classList.add("not-active");
+                element.classList.remove("active");
             }
         }
-	});
+    });
 };
 
 var onLongPress = function(element, callback) {
@@ -42,11 +52,11 @@ var onLongPress = function(element, callback) {
 };
 
 var spawnLittleRobot = function(x, y) {
-	var littleRobot = document.createElement("div");
-	littleRobot.id = "little-robot";
-	littleRobot.style.left = (x - 25) + "px";
-	littleRobot.style.top = (y - 25) + "px"; // grid cause grace said so nothx
-	return littleRobot;
+    var littleRobot = document.createElement("div");
+    littleRobot.id = "little-robot";
+    littleRobot.style.left = (x - 25) + "px";
+    littleRobot.style.top = (y - 25) + "px"; // grid cause grace said so nothx
+    return littleRobot;
 }
 
 var storeAction = function(name, value, x, y) {

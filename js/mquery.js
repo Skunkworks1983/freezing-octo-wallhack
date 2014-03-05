1d04f7831e4b376253e569367ca7422b1e2f3810
Skunk AWOL <skunk.awol@gmail.com>
2014-03-04 20:27:12 -0800

    update mquery

--- js/mquery.js
+++ js/mquery.js
@@ -19,14 +19,7 @@ Element.prototype.each = function(callback) {
     return this;
 };
 
-NodeList.prototype.on = function() {
-    var args = arguments;
-    this.each(function(element) {
-        element.on.apply(element, args);
-    });
-    return this;
-};
-
+// only one that can't be predefined
 NodeList.prototype.each = function(callback) {
     for (var i = 0; i < this.length; ++i) {
         var element = this[i];
@@ -35,26 +28,52 @@ NodeList.prototype.each = function(callback) {
     return this;
 };
 
+"on|dispatchEvent".split("|").forEach(function(functionName) {
+    NodeList.prototype[functionName] = function() {
+        var args = arguments;
+        this.each(function(element) {
+            element[functionName].apply(element, args);
+        });
+        return this;
+    };
+});
+
 // ajax
 
 var request = function(type, url, data, callback) {
     var xhr = new XMLHttpRequest(); // lol chrome
     var fd;
+    var posting = false;
+    var actualUrl = url;
     if (typeof data === "function") {
         callback = data;
         data = null;
     }
-    xhr.open(type, url);
-    if (type === "POST" && data != null && Object.keys(data).length != 0) {
-        fd = new FormData();
-        for (var key in data) {
-            fd.append(key, JSON.stringify(data[key]));
+    if (data != null && Object.keys(data).length != 0) {
+        if (type === "POST") {
+            posting = true;
+            fd = new FormData();
+            for (var key in data) {
+                fd.append(key, JSON.stringify(data[key]));
+            }
+        } else if (type === "GET") {
+            actualUrl = actualUrl + "?" + Object.keys(data).map(function(key) {
+                var val = key + "=";
+                if (Array.isArray(data[key])) {
+                    return data[key].map(function(v) {
+                        return val + JSON.stringify(v);
+                    }).join("&");
+                } else {
+                    return val + data[key];
+                }
+            }).join("&");
         }
     }
+    xhr.open(type, actualUrl);
     xhr.onload = function() {
         callback(JSON.parse(this.response));
     };
-    xhr.send((data != null) ? fd : null);
+    xhr.send(posting ? fd : null);
 };
 
 $.get = request.bind(global, "GET");
